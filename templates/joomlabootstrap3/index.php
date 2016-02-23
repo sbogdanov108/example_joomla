cead8c085c564125da262642a459c8472b54ce7f
Sergey Bogdanov <bgrodthai@gmail.com>
2016-02-23 11:33:24 +0300

    Сделал главную страницу

--- templates/joomlabootstrap3/index.php
+++ templates/joomlabootstrap3/index.php
@@ -61,57 +61,23 @@ $doc->addScript( $this->baseurl . '/templates/' . $this->template . '/js/app.js'
 
 <jdoc:include type="modules" name="carousel"/>
 
-<!-- Marketing messaging and featurettes
-================================================== -->
+<!-- ================================================== -->
 <!-- Wrap the rest of the page in another container to center all the content. -->
 
 <div class="container marketing">
 
   <jdoc:include type="modules" name="new-posts"/>
 
-  <!-- START THE FEATURETTES -->
-
   <jdoc:include type="message"/>
   <jdoc:include type="component"/>
-
-  <!--<hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">First featurette heading.
-        <span class="text-muted">It'll blow your mind.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
-        semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
-        commodo.</p>
-    </div>
-    <div class="col-md-5">
-      <img class="featurette-image img-responsive center-block" src="<?/*= $this->baseurl . '/templates/' . $this->template . '/images/img.png' */?>" alt="Generic placeholder image">
-    </div>
-  </div>
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7 col-md-push-5">
-      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span>
-      </h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
-        semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
-        commodo.</p>
-    </div>
-    <div class="col-md-5 col-md-pull-7">
-      <img class="featurette-image img-responsive center-block" src="<?/*= $this->baseurl . '/templates/' . $this->template . '/images/img.png' */?>" alt="Generic placeholder image">
-    </div>
-  </div>
-
-  <hr class="featurette-divider">-->
-
-  <!-- /END THE FEATURETTES -->
 
   <!-- FOOTER -->
   <footer>
     <p class="pull-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; <?= date( 'Y', time() ) ?> <?= htmlspecialchars( $config->get( 'sitename' ) ) ?> &middot;
+      <a href="#">Privacy</a> &middot;
+      <a href="#">Terms</a>
+    </p>
   </footer>
 
 </div><!-- /.container -->
