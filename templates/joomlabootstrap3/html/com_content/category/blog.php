bdb420360500e0a9b01d66187cf358325f4a06c4
Sergey Bogdanov <bgrodthai@gmail.com>
2016-02-24 14:33:57 +0300

    Сделал вывод bootstrap3 пагинации

--- templates/joomlabootstrap3/html/com_content/category/blog.php
+++ templates/joomlabootstrap3/html/com_content/category/blog.php
@@ -110,10 +110,6 @@ JHtml::_('behavior.caption');
 			<?php echo $this->loadTemplate('children'); ?> </div>
 	<?php endif; ?>
 	<?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
-		<div class="pagination">
-			<?php if ($this->params->def('show_pagination_results', 1)) : ?>
-				<p class="counter pull-right"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
-			<?php endif; ?>
-			<?php echo $this->pagination->getPagesLinks(); ?> </div>
+		<?php echo $this->pagination->getPagesLinks(); ?>
 	<?php endif; ?>
 </div>
